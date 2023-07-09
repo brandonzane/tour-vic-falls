eeecdf4665f7dcc47cd051dd2e35fb8f3aafd4e8
brandonzane <bmushori@gmail.com>
2023-07-09 21:16:48 +0200

    Updated routes and image uploads

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -1,12 +1,14 @@
 "use client";
 
+import React from "react"
+
 import { CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
 import { useCallback } from "react";
 import { TbPhotoPlus } from "react-icons/tb";
 
 declare global {
-  var cloudinary: any;
+  let cloudinary: any;
 }
 
 const uploadPreset = "pgc9ehd5";
