ff7ca820a53a56790259acaf6f4b47eeb0a241d2
brandonzane <bmushori@gmail.com>
2023-10-03 22:29:38 +0200

    Listing Reservation complete

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -1,10 +1,9 @@
 "use client";
-
-import React from "react";
 import { Range } from "react-date-range";
 import Calendar from "../inputs/Calendar";
+import Button from "../Button";
 
-interface ListingreservationProps {
+interface ListingReservationProps {
   price: number;
   dateRange: Range;
   totalPrice: number;
@@ -14,7 +13,7 @@ interface ListingreservationProps {
   disabledDates: Date[];
 }
 
-const ListingReservation: React.FC<ListingreservationProps> = ({
+const ListingReservation: React.FC<ListingReservationProps> = ({
   price,
   dateRange,
   totalPrice,
@@ -26,15 +25,10 @@ const ListingReservation: React.FC<ListingreservationProps> = ({
   return (
     <div
       className="
-    bg-white
-    rounded-xl
-    border-[1px]
-    border-neutral-200
-    overflow-hidden
-  "
+    bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden"
     >
       <div className="flex flex-row items-center gap-1 p-4">
-        <div className="text-2xl font-semibold">${price}</div>
+        <div className="text-2xl font-semibold">$ {price}</div>
         <div className="font-light text-neutral-600">night</div>
       </div>
       <hr />
@@ -43,6 +37,14 @@ const ListingReservation: React.FC<ListingreservationProps> = ({
         disabledDates={disabledDates}
         onChange={(value) => onChangeDate(value.selection)}
       />
+      <hr />
+      <div className="p-4">
+        <Button disabled={disabled} label="Reserve" onClick={onSubmit} />
+      </div>
+      <div className="p-4 flex flex-row items-center justify-between font-semibold text-lg">
+        <div>Total</div>
+        <div>$ {totalPrice}</div>
+      </div>
     </div>
   );
 };
