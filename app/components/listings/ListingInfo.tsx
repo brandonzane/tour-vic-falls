e218879e8aa529d713240e27332ffd43eec66888
brandonzane <bmushori@gmail.com>
2023-10-25 15:11:18 +0200

    Fixed Map component type error

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -1,24 +1,22 @@
 "use client";
 
-import dynamic from "next/dynamic";
+import { FC } from "react";
 import { IconType } from "react-icons";
-
-import useCountries from "@/app/hooks/useCountries";
-import { SafeUser } from "@/app/types";
-
+import useCountries from "../../hooks/useCountries";
+import { SafeUser } from "../../types";
 import Avatar from "../Avatar";
 import ListingCategory from "./ListingCategory";
+import dynamic from "next/dynamic";
 
-const Map = dynamic(() => import("../Map"), {
-  ssr: false,
-});
+const Map = dynamic(() => import("../Map"), { ssr: false });
 
 interface ListingInfoProps {
   user: SafeUser;
   description: string;
-  guestCount: number;
   roomCount: number;
+  guestCount: number;
   bathroomCount: number;
+  locationValue: string;
   category:
     | {
         icon: IconType;
@@ -26,17 +24,16 @@ interface ListingInfoProps {
         description: string;
       }
     | undefined;
-  locationValue: string;
 }
 
-const ListingInfo: React.FC<ListingInfoProps> = ({
+const ListingInfo: FC<ListingInfoProps> = ({
   user,
   description,
-  guestCount,
   roomCount,
+  guestCount,
   bathroomCount,
-  category,
   locationValue,
+  category,
 }) => {
   const { getByValue } = useCountries();
 
@@ -45,50 +42,37 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
   return (
     <div className="col-span-4 flex flex-col gap-8">
       <div className="flex flex-col gap-2">
-        <div
-          className="
-            text-xl 
-            font-semibold 
-            flex 
-            flex-row 
-            items-center
-            gap-2
-          "
-        >
+        <div className="text-xl font-semibold flex flex-row items-center gap-2">
           <div>Hosted by {user?.name}</div>
+
           <Avatar src={user?.image} />
         </div>
-        <div
-          className="
-            flex 
-            flex-row 
-            items-center 
-            gap-4 
-            font-light
-            text-neutral-500
-          "
-        >
+
+        <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
           <div>{guestCount} guests</div>
+
           <div>{roomCount} rooms</div>
+
           <div>{bathroomCount} bathrooms</div>
         </div>
       </div>
+
       <hr />
+
       {category && (
         <ListingCategory
           icon={category.icon}
-          label={category?.label}
-          description={category?.description}
+          label={category.label}
+          description={category.description}
         />
       )}
+
       <hr />
-      <div
-        className="
-      text-lg font-light text-neutral-500"
-      >
-        {description}
-      </div>
+
+      <div className="text-lg font-light text-neutral-500">{description}</div>
+
       <hr />
+
       <Map center={coordinates} />
     </div>
   );
