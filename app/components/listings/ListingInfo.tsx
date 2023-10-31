8429142b3eb543e42bf77d645401d9f19a0d472b
brandonzane <bmushori@gmail.com>
2023-10-31 16:36:09 +0200

    Added react path in tsconfig file

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -42,45 +42,41 @@ const ListingInfo: FC<ListingInfoProps> = ({
   const coordinates = getByValue(locationValue)?.latlng;
 
   return (
-    <>
-      (
-      <div className="col-span-4 flex flex-col gap-8">
-        <div className="flex flex-col gap-2">
-          <div className="text-xl font-semibold flex flex-row items-center gap-2">
-            <div>Hosted by {user?.name}</div>
+    <div className="col-span-4 flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
+        <div className="text-xl font-semibold flex flex-row items-center gap-2">
+          <div>Hosted by {user?.name}</div>
 
-            <Avatar src={user?.image} />
-          </div>
-
-          <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
-            <div>{guestCount} guests</div>
-
-            <div>{roomCount} rooms</div>
-
-            <div>{bathroomCount} bathrooms</div>
-          </div>
+          <Avatar src={user?.image} />
         </div>
 
-        <hr />
+        <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
+          <div>{guestCount} guests</div>
 
-        {category && (
-          <ListingCategory
-            icon={category.icon}
-            label={category.label}
-            description={category.description}
-          />
-        )}
+          <div>{roomCount} rooms</div>
 
-        <hr />
-
-        <div className="text-lg font-light text-neutral-500">{description}</div>
-
-        <hr />
-
-        <Map center={coordinates} />
+          <div>{bathroomCount} bathrooms</div>
+        </div>
       </div>
-      )
-    </>
+
+      <hr />
+
+      {category && (
+        <ListingCategory
+          icon={category.icon}
+          label={category.label}
+          description={category.description}
+        />
+      )}
+
+      <hr />
+
+      <div className="text-lg font-light text-neutral-500">{description}</div>
+
+      <hr />
+
+      <Map center={coordinates} />
+    </div>
   );
 };
 
