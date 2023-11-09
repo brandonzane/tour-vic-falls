7328f783a09b3103fe0f5ef4644b6e6b08ac5316
brandonzane <bmushori@gmail.com>
2023-11-09 14:04:26 +0200

    Update Loader component with BounceLoader

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { PuffLoader } from "react-spinners";
+import { BounceLoader } from "react-spinners";
 
 const Loader = () => {
   return (
@@ -13,7 +13,7 @@ const Loader = () => {
       items-center 
     "
     >
-      <PuffLoader size={100} color="blue" />
+      <BounceLoader size={100} color="blue" />
     </div>
   );
 };
