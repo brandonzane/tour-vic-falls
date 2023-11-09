4a2e1d7c5ba02752042ac1c06194093067ffe68f
brandonzane <bmushori@gmail.com>
2023-11-09 11:03:18 +0200

    Refactor DELETE function in listing route to include currentUser check

--- app/api/listings/[listingId]/route.ts
+++ app/api/listings/[listingId]/route.ts
@@ -3,15 +3,18 @@ import { NextResponse } from "next/server";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import prisma from "@/app/lib/prismadb";
 
-interface IPrams {
+interface IParams {
   listingId?: string;
 }
 
-export async function DELETE(request: Request, { params }: { params: IPrams }) {
+export async function DELETE(
+  request: Request,
+  { params }: { params: IParams }
+) {
   const currentUser = await getCurrentUser();
 
   if (!currentUser) {
-    return NextResponse.error;
+    return NextResponse.error();
   }
 
   const { listingId } = params;
